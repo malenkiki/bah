a76bb2e60bce25d538da54df22dd711ddb9080aa
Michel Petit <petit.michel@gmail.com>
2014-05-07 13:56:37 +0200

    Arabic number (not UT yet)

--- src/Malenki/Bah/N.php
+++ src/Malenki/Bah/N.php
@@ -81,6 +81,14 @@ class N
         if ($name == 'hindi') {
             return $this->hindi();
         }
+        
+        if ($name == 'arabic') {
+            return $this->arabic();
+        }
+        
+        if ($name == 'iperso_arabic') {
+            return $this->persoArabic();
+        }
     }
 
     public function __construct($num = 0)
@@ -886,16 +894,72 @@ class N
         return new S($str);
     }
 
+    protected function _arabic($type = 'eastern')
+    {
+        $arr = array(
+            0 => '٠',
+            1 => '١',
+            2 => '٢',
+            3 => '٣',
+            4 => '٤',
+            5 => '٥',
+            6 => '٦',
+            7 => '٧',
+            8 => '٨',
+            9 => '٩'
+        );
+
+        if($type == 'persoarabic'){
+            $arr = array(
+                0 => '۰',
+                1 => '۱',
+                2 => '۲',
+                3 => '۳',
+                4 => '۴',
+                5 => '۵',
+                6 => '۶',
+                7 => '۷',
+                8 => '۸',
+                9 => '۹'
+            );
+        }
+
+        //TODO
+        if($type == 'pakistani'){
+            $arr = array(
+
+            );
+        }
+        
+        return new S(strtr((string) $this->value, $arr));
+    }
+
+
+    public function arabic()
+    {
+        return $this->_arabic();
+    }
+
+    public function persoArabic()
+    {
+        return $this->_arabic('persoarabic');
+    }
+
+    public function pakistani()
+    {
+        throw new \RuntimeException('Pakistani conversion is not available yet');
+        return $this->_arabic('pakistani');
+    }
 
 
     /**
-     * Convert to arabian string number
+     * Convert to arabian string number (equiv of roman)
      *
      * @todo to implement
      * @access public
      * @return S
      */
-    public function arabian()
+    public function abjad()
     {
     }
 
