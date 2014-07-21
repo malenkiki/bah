f28d7346ce7a5c38b775057d3c8d514213fd20c2
Michel Petit <petit.michel@gmail.com>
2014-07-21 14:11:46 +0200

    Updated doc

--- doc/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Malenki\\Bah\\A"],["c","Malenki\\Bah\\C"],["c","Malenki\\Bah\\H"],["c","Malenki\\Bah\\N"],["c","Malenki\\Bah\\O"],["c","Malenki\\Bah\\S"]];
